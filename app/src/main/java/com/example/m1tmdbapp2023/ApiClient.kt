0893e3978110cb4e03f796196919e4418ee418eb
lpastyle <lpastorelli@lpastyle.com>
2023-03-09 12:45:09 +0100

    Added Retrofit init

--- app/src/main/java/com/example/m1tmdbapp2023/ApiClient.kt
+++ app/src/main/java/com/example/m1tmdbapp2023/ApiClient.kt
@@ -1,4 +1,42 @@
 package com.example.m1tmdbapp2023
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 class ApiClient {
+
+    /* companion is used to get the JAVA "static" behavior we need to implement a singleton pattern
+      * see https://kotlinlang.org/docs/object-declarations.html#companion-objects
+      */
+   companion object {
+       const val TMDBAPI_BASE_URL = "https://api.themoviedb.org/3/"
+
+       // keep singleton instance
+       val instance = Build()
+
+       private fun Build(): Retrofit {
+
+           // GSON converter creation
+           val converter = GsonConverterFactory.create()
+
+           val httpLoggingInterceptor = HttpLoggingInterceptor()
+           httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC)
+           // HTTP Client
+
+           val okHttpClient = OkHttpClient().newBuilder()
+               .readTimeout(30, TimeUnit.SECONDS)
+               .writeTimeout(30, TimeUnit.SECONDS)
+               .addInterceptor(httpLoggingInterceptor)
+               .build()
+
+           return Retrofit.Builder()
+               .baseUrl(TMDBAPI_BASE_URL)
+               .addConverterFactory(converter)
+               .client(okHttpClient)
+               .build()
+       }
+   }
 }
