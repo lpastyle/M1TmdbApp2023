a78a40eca326e96225a421e8c5025844b72c6d32
lpastyle <lpastorelli@lpastyle.com>
2023-03-09 14:23:51 +0100

    Added call to getPopularPerson

--- app/src/main/java/com/example/m1tmdbapp2023/MainActivity.kt
+++ app/src/main/java/com/example/m1tmdbapp2023/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.m1tmdbapp2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    val LOGTAG = MainActivity::class.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tmdbapi = ApiClient.instance.create(ITmdbApi::class.java)
+
+        val call = tmdbapi.getPopularPerson(TMDB_API_KEY, 1)
+
+        call.enqueue(object : Callback<PersonPopularResponse> {
+            override fun onResponse(
+                call: Call<PersonPopularResponse>,
+                response: Response<PersonPopularResponse>
+            ) {
+                Log.d(LOGTAG, response.body().toString())
+            }
+
+            override fun onFailure(call: Call<PersonPopularResponse>, t: Throwable) {
+                Log.e(LOGTAG,"Call to getPopularPerson failed")
+            }
+        }  )
     }
 }
