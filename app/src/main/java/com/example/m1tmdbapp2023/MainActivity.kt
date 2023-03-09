09cb8774de44242f4ca18f52617cf7808b42f2ff
lpastyle <lpastorelli@lpastyle.com>
2023-03-09 16:30:14 +0100

    Added adapter class

--- app/src/main/java/com/example/m1tmdbapp2023/MainActivity.kt
+++ app/src/main/java/com/example/m1tmdbapp2023/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.m1tmdbapp2023
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.m1tmdbapp2023.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -10,27 +12,45 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
     val LOGTAG = MainActivity::class.simpleName
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var personPopularAdapter: PersonPopularAdapter
+    private var persons = arrayListOf<Person>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // Init recycler view
+        binding.popularPersonRv.setHasFixedSize(true)
+        binding.popularPersonRv.layoutManager = LinearLayoutManager(this)
+        personPopularAdapter = PersonPopularAdapter()
+        binding.popularPersonRv.adapter = personPopularAdapter
 
+        loadPage(1)
 
+    }
+
+    private fun loadPage(page: Int) {
         val tmdbapi = ApiClient.instance.create(ITmdbApi::class.java)
 
-        val call = tmdbapi.getPopularPerson(TMDB_API_KEY, 1)
+        val call = tmdbapi.getPopularPerson(TMDB_API_KEY, page)
 
         call.enqueue(object : Callback<PersonPopularResponse> {
             override fun onResponse(
                 call: Call<PersonPopularResponse>,
                 response: Response<PersonPopularResponse>
             ) {
-                Log.d(LOGTAG, response.body().toString())
+                if (response.isSuccessful) {
+                    persons.addAll(response.body()?.results!!)
+                } else {
+                    Log.e(LOGTAG, "Call to getPopularPerson failed with error ${response.code()}")
+                }
             }
 
             override fun onFailure(call: Call<PersonPopularResponse>, t: Throwable) {
                 Log.e(LOGTAG,"Call to getPopularPerson failed")
             }
-        }  )
+        })
     }
 }
---
+++ app/src/main/java/com/example/m1tmdbapp2023/PersonPopularAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.m1tmdbapp2023
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PersonPopularAdapter : RecyclerView.Adapter<PersonPopularAdapter.ViewHolder>(){
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
