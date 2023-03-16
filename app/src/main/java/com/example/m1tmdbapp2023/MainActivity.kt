e88a782c9f713fe15908c81b65c1b7144b182041
lpastyle <lpastorelli@lpastyle.com>
2023-03-16 10:29:26 +0100

    updated progress wheel

--- app/src/main/java/com/example/m1tmdbapp2023/MainActivity.kt
+++ app/src/main/java/com/example/m1tmdbapp2023/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.m1tmdbapp2023
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.m1tmdbapp2023.databinding.ActivityMainBinding
 import retrofit2.Call
@@ -10,11 +12,15 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
     val LOGTAG = MainActivity::class.simpleName
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var personPopularAdapter: PersonPopularAdapter
     private var persons = arrayListOf<Person>()
+    private var totalResults = 0
+    private var totalPages = Int.MAX_VALUE
+    private var curPage = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         personPopularAdapter = PersonPopularAdapter(persons)
         binding.popularPersonRv.adapter = personPopularAdapter
 
-        loadPage(1)
+        loadPage(curPage)
 
     }
 
@@ -35,22 +41,27 @@ class MainActivity : AppCompatActivity() {
         val tmdbapi = ApiClient.instance.create(ITmdbApi::class.java)
 
         val call = tmdbapi.getPopularPerson(TMDB_API_KEY, page)
-
+        binding.progressWheel.visibility = VISIBLE
         call.enqueue(object : Callback<PersonPopularResponse> {
             override fun onResponse(
                 call: Call<PersonPopularResponse>,
                 response: Response<PersonPopularResponse>
             ) {
-                if (response.isSuccessful) {
+                if (response.isSuccessful && response.body() != null) {
                     persons.addAll(response.body()?.results!!)
+                    totalResults = response.body()!!.totalResults!!
+                    totalPages = response.body()!!.totalPages!!
                     personPopularAdapter.notifyDataSetChanged()
+                    binding.totalResultsTv.text = getString(R.string.total_results_text,persons.size, totalResults)
                 } else {
                     Log.e(LOGTAG, "Call to getPopularPerson failed with error ${response.code()}")
                 }
+                binding.progressWheel.visibility = GONE
             }
 
             override fun onFailure(call: Call<PersonPopularResponse>, t: Throwable) {
                 Log.e(LOGTAG,"Call to getPopularPerson failed")
+                binding.progressWheel.visibility = GONE
             }
         })
     }
