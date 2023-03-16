e8a76f4d8ff2d865a4b9555ed9bdc1a040c0229b
lpastyle <lpastorelli@lpastyle.com>
2023-03-16 12:57:24 +0100

    Added custom view parameter init

--- app/src/main/java/com/example/m1tmdbapp2023/ScoreGaugeView.kt
+++ app/src/main/java/com/example/m1tmdbapp2023/ScoreGaugeView.kt
@@ -17,13 +17,18 @@ class ScoreGaugeView @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
 
     // hold XML custom attributes
-    private var scoreMax:Int = 100
+    private var scoreMax:Int
     private var scoreValue:Float = 75f
     private var scoreLabel:String = "Preview"
     private var scoreColor:Int = Color.RED
 
     init {
-
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ScoreGaugeView)
+        scoreMax = typedArray.getInt(R.styleable.ScoreGaugeView_scoreMax,100)
+        scoreValue = typedArray.getFloat(R.styleable.ScoreGaugeView_scoreValue, 0f)
+        scoreLabel = typedArray.getString(R.styleable.ScoreGaugeView_scoreLabel)                  .toString()
+        scoreColor = typedArray.getColor(R.styleable.ScoreGaugeView_scoreColor, 0)
+        typedArray.recycle()
     }
 
 }
