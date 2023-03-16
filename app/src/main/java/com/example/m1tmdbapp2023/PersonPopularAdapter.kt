8f7384c7e0cb17153ae7b9d14ac589883d0ea84d
lpastyle <lpastorelli@lpastyle.com>
2023-03-16 16:43:32 +0100

    Added bind for view holder

--- app/src/main/java/com/example/m1tmdbapp2023/PersonPopularAdapter.kt
+++ app/src/main/java/com/example/m1tmdbapp2023/PersonPopularAdapter.kt
@@ -1,61 +1,49 @@
 package com.example.m1tmdbapp2023
 
 import android.view.LayoutInflater
-import android.view.PixelCopy
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.m1tmdbapp2023.ApiClient.Companion.IMAGE_BASE_URL
 import com.squareup.picasso.Picasso
 import android.content.Context
 import android.graphics.Color
+import com.example.m1tmdbapp2023.databinding.PersonItemBinding
 
-class PersonPopularAdapter(val persons: ArrayList<Person>, context: Context) : RecyclerView.Adapter<PersonPopularAdapter.ViewHolder>(){
+class PersonPopularAdapter(val persons: ArrayList<Person>, context: Context) : RecyclerView.Adapter<PersonPopularAdapter.PersonItemViewHolder>(){
+
+    /**
+     * Provide a reference to the type of views that you are using (custom ViewHolder)
+     */
+    class PersonItemViewHolder(var binding: PersonItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     private var maxPopularity:Double = 0.0
     private val scoreRatings: Array<String> = context.resources.getStringArray(R.array.score_rating)
     private val ratingColors: Array<String> = context.resources.getStringArray(R.array.rating_colors)
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val nameTv: TextView
-        val knownForTv: TextView
-        val popularityTv: TextView
-        val photoIv: ImageView
-        val scoreGaugeView: ScoreGaugeView
 
-        init {
-            nameTv = view.findViewById(R.id.name_tv)
-            knownForTv = view.findViewById(R.id.known_for_tv)
-            popularityTv = view.findViewById(R.id.popularity_tv)
-            photoIv = view.findViewById(R.id.photo_iv)
-            scoreGaugeView = view.findViewById(R.id.score_gauge_view)
-        }
-    }
     init {
         setMaxPopularity()
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.person_item, parent, false)
-        return ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonItemViewHolder {
+        val binding = PersonItemBinding.inflate((LayoutInflater.from(parent.context)), parent, false)
+        return PersonItemViewHolder(binding)
     }
 
     override fun getItemCount() = persons.size
 
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: PersonItemViewHolder, position: Int) {
         val curItem = persons.get(position)
-        holder.nameTv.text = curItem.name
-        holder.knownForTv.text = curItem.knownForDepartment
-        holder.popularityTv.text = curItem.popularity.toString()
+        holder.binding.nameTv.text = curItem.name
+        holder.binding.knownForTv.text = curItem.knownForDepartment
+        holder.binding.popularityTv.text = curItem.popularity.toString()
         Picasso.get()
             .load(IMAGE_BASE_URL + curItem.profilePath)
             .placeholder(android.R.drawable.progress_horizontal)
             .error(android.R.drawable.stat_notify_error)
-            .into(holder.photoIv)
+            .into(holder.binding.photoIv)
 
-        holder.scoreGaugeView.updateScore(
+        holder.binding.scoreGaugeView.updateScore(
             getRating(curItem.popularity,maxPopularity),
             getScoreColor(curItem.popularity,maxPopularity),
             curItem.popularity!!.toFloat(),
@@ -83,4 +71,20 @@ class PersonPopularAdapter(val persons: ArrayList<Person>, context: Context) : R
             Color.RED
         }
     }
+
+    /* class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val nameTv: TextView
+        val knownForTv: TextView
+        val popularityTv: TextView
+        val photoIv: ImageView
+        val scoreGaugeView: ScoreGaugeView
+
+        init {
+            nameTv = view.findViewById(R.id.name_tv)
+            knownForTv = view.findViewById(R.id.known_for_tv)
+            popularityTv = view.findViewById(R.id.popularity_tv)
+            photoIv = view.findViewById(R.id.photo_iv)
+            scoreGaugeView = view.findViewById(R.id.score_gauge_view)
+        }
+    }*/
 }
