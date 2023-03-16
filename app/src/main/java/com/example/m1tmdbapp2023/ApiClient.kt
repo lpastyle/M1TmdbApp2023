0959deb39bcf8267ab47f8366c3460cad19755ab
lpastyle <lpastorelli@lpastyle.com>
2023-03-16 09:43:24 +0100

    added text and color res

--- app/src/main/java/com/example/m1tmdbapp2023/ApiClient.kt
+++ app/src/main/java/com/example/m1tmdbapp2023/ApiClient.kt
@@ -8,11 +8,11 @@ import java.util.concurrent.TimeUnit
 
 class ApiClient {
 
-    /* companion is used to get the JAVA "static" behavior we need to implement a singleton pattern
-      * see https://kotlinlang.org/docs/object-declarations.html#companion-objects
-      */
+   /* companion is used to get the JAVA "static" behavior we need to implement a singleton pattern
+    * see https://kotlinlang.org/docs/object-declarations.html#companion-objects for details
+    */
    companion object {
-       const val TMDBAPI_BASE_URL = "https://api.themoviedb.org/3/"
+        const val TMDBAPI_BASE_URL = "https://api.themoviedb.org/3/"
         const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w45"
 
        // keep singleton instance
