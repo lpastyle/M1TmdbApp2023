f2ea96a78379d883a0a3afe9cbd855d65791d59a
lpastyle <lpastorelli@lpastyle.com>
2023-03-23 12:41:13 +0100

    Added notif permission check

--- app/src/main/java/com/example/m1tmdbapp2023/MainActivity.kt
+++ app/src/main/java/com/example/m1tmdbapp2023/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.m1tmdbapp2023
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,6 +13,8 @@ import android.view.View.VISIBLE
 import android.widget.AbsListView
 import android.widget.AbsListView.OnScrollListener
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.SCROLL_STATE_IDLE
@@ -24,13 +28,34 @@ const val NOTIFICATION_CHANNEL_ID = "popular_person_notification_channel_id"
 class MainActivity : AppCompatActivity() {
 
     val LOGTAG = MainActivity::class.simpleName
-
+    private var isNotifPermGranted = false // TODO: replace with SHARED PREFERENCE
     private lateinit var binding: ActivityMainBinding
     private lateinit var personPopularAdapter: PersonPopularAdapter
     private var persons = arrayListOf<Person>()
     private var totalResults = 0
     private var totalPages = Int.MAX_VALUE
     private var curPage = 1
+
+    // Register the permissions callback, which handles the user's response to the
+    // system permissions dialog. Save the return value, an instance of
+    // ActivityResultLauncher. You can use either a val, as shown in this snippet,
+    // or a lateinit var in your onAttach() or onCreate() method.
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission())
+        { isGranted: Boolean ->
+            isNotifPermGranted = isGranted
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your app.
+                Log.i(LOGTAG,"Permission was granted")
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // feature requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+                Log.i(LOGTAG, "Permission was denied" )
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,7 +79,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         })
-
+        check4NotificationPermission()
         loadPage(curPage)
 
     }
@@ -89,6 +114,28 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
+    private fun check4NotificationPermission():Any = when {
+        ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                == PackageManager.PERMISSION_GRANTED -> {
+            Log.i(LOGTAG, "notification permission already granted")
+            isNotifPermGranted = true
+        }
+        shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected, and what
+            // features are disabled if it's declined. In this UI, include a
+            // "cancel" or "no thanks" button that lets the user continue
+            // using your app without granting the permission.
+            Log.i(LOGTAG, "show a permission rationale explanation dialog")
+        }
+        else -> {
+            // You can directly ask for the permission.
+            // The registered ActivityResultCallback gets the result of this request.
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+    }
+
 
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
