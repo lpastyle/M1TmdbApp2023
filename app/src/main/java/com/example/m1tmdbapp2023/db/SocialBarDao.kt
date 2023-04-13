1b68097375e791d6c4e536072d7b11b857ef5431
lpastyle <lpastorelli@lpastyle.com>
2023-04-13 14:20:53 +0200

    adder @mapinfo in DAO

--- app/src/main/java/com/example/m1tmdbapp2023/db/SocialBarDao.kt
+++ app/src/main/java/com/example/m1tmdbapp2023/db/SocialBarDao.kt
@@ -1,15 +1,14 @@
 package com.example.m1tmdbapp2023.db
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface SocialBarDao {
+    @MapInfo(keyColumn = "person_id", valueColumn = "is_favorite")
     @Query("SELECT person_id, is_favorite FROM social_bar_table")
     fun getAllFavorites(): MutableMap<Int,Boolean>
 
+    @MapInfo(keyColumn = "person_id", valueColumn = "nb_likes")
     @Query("SELECT person_id, nb_likes FROM social_bar_table")
     fun getAllLikes(): MutableMap<Int,Int>
 
