4491ea5a8a3a2adcc1500e304c712a386f4ed993
lpastyle <lpastorelli@lpastyle.com>
2023-04-28 16:28:31 +0200

    Added sensor demo dimens

--- app/src/main/java/com/example/m1tmdbapp2023/SensorDemoActivity.kt
+++ app/src/main/java/com/example/m1tmdbapp2023/SensorDemoActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SensorDemoActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sensor_demo)
